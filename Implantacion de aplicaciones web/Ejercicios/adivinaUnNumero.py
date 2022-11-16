607cccd7d5df75b5e8e55819cbb594d2eeacb52b
[cristianotermin99] <[cristianotermin99@gmail.com]>
2022-11-16 11:54:13 +0100

    adivina numero acabado

--- Implantacion de aplicaciones web/Ejercicios/adivinaUnNumero.py
+++ Implantacion de aplicaciones web/Ejercicios/adivinaUnNumero.py
@@ -7,31 +7,30 @@ dineroInicialJugador=0
 dineroInicialJugador=int(input("Introduce la cantidad de dinero inicial con la que vas a jugar: "))
 dineroFinalJugador=dineroInicialJugador
 dineroApostadoEstaPartida=0
-
-
-while numeroAleatorio != numeroIntroducido and numeroIntentos<10:
-    dineroApostadoEstaPartida=int(input("Introduce la cantidad de dinero que quieres apostar: "))
+seguir=1
+while seguir==1:
+    dineroApostadoEstaPartida=int(input("Introduce la cantidad de dinero que quieres apostar esta partida: "))
     if dineroApostadoEstaPartida<dineroFinalJugador:
-        numeroIntroducido=int(input("Introduce un numero entre 1 y 100: "))
-        numeroIntentos+=1
-        if numeroIntroducido<numeroAleatorio:
-            print("El numero que has introducido es menor. Introduce un numero mayor. Llevas ", numeroIntentos, " intentos")
-        elif numeroIntroducido>numeroAleatorio:
-            print("El numero que has introducido es mayor. Introduce un numero menor. Llevas ", numeroIntentos, " intentos")
+        while numeroAleatorio != numeroIntroducido and numeroIntentos<10:
+        
+            numeroIntroducido=int(input("Introduce un numero entre 1 y 100: "))
+            numeroIntentos+=1
+            if numeroIntroducido<numeroAleatorio:
+                print("El numero que has introducido es menor. Introduce un numero mayor. Llevas ", numeroIntentos, " intentos")
+            elif numeroIntroducido>numeroAleatorio:
+                print("El numero que has introducido es mayor. Introduce un numero menor. Llevas ", numeroIntentos, " intentos")
+
+        if numeroAleatorio==numeroIntroducido:
+            dineroFinalJugador+=dineroApostadoEstaPartida
+            print("Has acertado el numero era ", numeroAleatorio)
+            print("Has ganado ",dineroApostadoEstaPartida, " euros") 
+        else:
+            dineroFinalJugador-=dineroApostadoEstaPartida
+            print("Has perdido con 10 fallos")
+            print("Has perdido ",dineroApostadoEstaPartida, " euros")
     else:
         print("No puedes apostar mas dinero del que tienes! ")
 
-
-if numeroAleatorio==numeroIntroducido:
-    dineroFinalJugador+=dineroApostadoEstaPartida
-    print("Has acertado el numero era ", numeroAleatorio)
-    print("Has ganado ",dineroApostadoEstaPartida, " euros") 
-else:
-    dineroFinalJugador-=dineroApostadoEstaPartida
-    print("Has perdido con 10 fallos")
-    print("Has perdido ",dineroApostadoEstaPartida, " euros")
-
-
-
-print("Tienes ",dineroFinalJugador, " euros")  
+    print("Tienes ",dineroFinalJugador, " euros")  
+    seguir=int(input("Deseas continuar jugando? 1. Si    2.No:    "))
 
